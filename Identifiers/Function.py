445b2a626ef4b0dd3b5439c63a9acbff274bd35a
GOPICHAND-KAMEPALLI <kamepalligopichand@gmail.com>
2023-08-02 16:15:59 +0530

    Identifiers

---
+++ Identifiers/Function.py
@@ -0,0 +1,13 @@
+#single Line comments
+'''multi line
+comments'''
+id=101
+ename="Rahul"
+ 
+def add():
+     pass
+
+def Employee():
+    pass
+print(ename)
+print(id)
--- Two.py
+++
@@ -1,6 +0,0 @@
-id=101
-Ename="Gopichand"
-salary=45000.35
-print(id)
-print(Ename)
-print(salary)
