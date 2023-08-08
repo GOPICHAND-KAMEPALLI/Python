f3c354065a6cf9339b5201615b391ea38be3c4df
GOPICHAND-KAMEPALLI <kamepalligopichand@gmail.com>
2023-08-08 11:23:40 +0530

    List

---
+++ Iterative Flow 2/Creating List.py
@@ -0,0 +1,3 @@
+#Creating List Objects
+sizes=[38,40,42,44]    
+print(type(sizes))
---
+++ Iterative Flow 2/Reading List.py
@@ -0,0 +1,9 @@
+#Creating List Objects
+sizes=[38,40,42,44]    
+#Reading List Objects
+print(sizes)
+# Reading List Objects Using Indexing
+print(sizes[0])
+print(sizes[1])
+print(sizes[2])
+print(sizes[3])
